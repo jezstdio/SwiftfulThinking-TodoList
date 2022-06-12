7b2c9dc49d264717342b3ed504cb4b564f44a0c7
jezstdio <jezstdio@protonmail.com>
2022-06-12 11:06:29 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jez Gábor on 2022. 06. 12..
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
